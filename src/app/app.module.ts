43a6fdba4359b2a83c5b7702f9775fb6127c61c1
Dorota Broszko <broszko.dorota.e@gmail.com>
2024-06-12 14:22:33 +0200

    pierwsze zmiany

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent,
+    LoginComponent,
+    RegisterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
